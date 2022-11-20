199ceda4cd64965f664383f316b35784874d41bc
zekigklr <77938037+zekigklr@users.noreply.github.com>
2022-11-21 00:59:09 +0300

    Update
    My mistake

--- Project1/Project1/ViewController.swift
+++ Project1/Project1/ViewController.swift
@@ -2,51 +2,46 @@
 //  ViewController.swift
 //  Project1
 //
-//  Created by Zeki Gökler on 15.11.2022.
+//  Created by TwoStraws on 11/08/2016.
+//  Copyright © 2016 Paul Hudson. All rights reserved.
 //
 
 import UIKit
 
 class ViewController: UITableViewController {
-    var pictures = [String]()
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
-        title = "Storm Viewer"
+	var pictures = [String]()
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+
+		title = "Storm Viewer"
         navigationController?.navigationBar.prefersLargeTitles = true
-        
-        let fm = FileManager.default
-        let path = Bundle.main.resourcePath!
-        let items = try! fm.contentsOfDirectory(atPath: path)
-        
-        
-        for item in items {
-            if item.hasPrefix("nssl"){
-                pictures.append(item)
-            }
-        }
-        
-        pictures.sort()
-    }
 
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return pictures.count
-    }
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "Picture", for: indexPath)
-        cell.textLabel?.text = pictures[indexPath.row]
-        return cell
-    }
-    
-    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        if let vc = storyboard?.instantiateViewController(withIdentifier: "Detail") as? DetailViewController {
-            vc.selectedImage = pictures[indexPath.row]
-            vc.titleString = "Picture \(indexPath.row + 1) of \(pictures.count)"
-            navigationController?.pushViewController(vc, animated: true)
-        }
-    }
-    
-    
+		let fm = FileManager.default
+		let path = Bundle.main.resourcePath!
+		let items = try! fm.contentsOfDirectory(atPath: path)
+
+		for item in items {
+			if item.hasPrefix("nssl") {
+				pictures.append(item)
+			}
+		}
+	}
+
+	override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+		return pictures.count
+	}
+
+	override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+		let cell = tableView.dequeueReusableCell(withIdentifier: "Picture", for: indexPath)
+		cell.textLabel?.text = pictures[indexPath.row]
+		return cell
+	}
+
+	override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+		if let vc = storyboard?.instantiateViewController(withIdentifier: "Detail") as? DetailViewController {
+			vc.selectedImage = pictures[indexPath.row]
+			navigationController?.pushViewController(vc, animated: true)
+		}
+	}
 }
-
---
+++ Project3/Project1/DetailViewController.swift
@@ -0,0 +1,61 @@
+//
+//  DetailViewController.swift
+//  Project1
+//
+//  Created by Zeki Gökler on 15.11.2022.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+    @IBOutlet var imageView: UIImageView!
+    var selectedImage: String?
+        var titleString: String?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        title = titleString
+        navigationItem.largeTitleDisplayMode = .never
+        
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .action, target: self, action: #selector(shareTapped))
+
+        if let imageToLoad = selectedImage {
+            imageView.image = UIImage(named: imageToLoad)
+        }
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        navigationController?.hidesBarsOnTap = true
+    }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+        navigationController?.hidesBarsOnTap = false
+    }
+    @objc func shareTapped() {
+        guard let image = imageView.image?.jpegData(compressionQuality: 0.8)
+        else {
+            print("No image found")
+            return
+        }
+        
+        let vc = UIActivityViewController(activityItems: [image], applicationActivities: [])
+        vc.popoverPresentationController?.barButtonItem = navigationItem.rightBarButtonItem
+        present(vc, animated: true)
+    }
+    
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
