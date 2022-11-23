28118707d44da0e607853820d4277be3684d1af7
zekigklr <77938037+zekigklr@users.noreply.github.com>
2022-11-23 18:11:25 +0300

    No update

--- Project4/Project4/ViewController.swift
+++ Project4/Project4/ViewController.swift
@@ -81,7 +81,9 @@ class ViewController: UIViewController, WKNavigationDelegate {
 
     override func observeValue(forKeyPath keyPath: String?, of object: Any?, change: [NSKeyValueChangeKey : Any]?, context: UnsafeMutableRawPointer?) {
         if keyPath == "estimatedProgress" {
+            
             progressView.progress = Float(webView.estimatedProgress)
+            
         }
     }
 }
