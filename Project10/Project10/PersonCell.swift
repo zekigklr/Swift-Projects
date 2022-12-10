2c17856548ee5ed3d8698ac3c09400d91c031a2d
zekigklr <77938037+zekigklr@users.noreply.github.com>
2022-12-10 17:10:08 +0300

    Project-10
    I'm starting project 10.

---
+++ Project10/Project10/PersonCell.swift
@@ -0,0 +1,15 @@
+//
+//  PersonCell.swift
+//  Project10
+//
+//  Created by Zeki Gökler on 10.12.2022.
+//
+
+import UIKit
+
+class PersonCell: UICollectionViewCell {
+    @IBOutlet var imageView: UIImageView!
+    @IBOutlet var name: UILabel!
+    
+    
+}
---
+++ Project10/Project10/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Project10
+//
+//  Created by Zeki Gökler on 10.12.2022.
+//
+
+import UIKit
+
+class ViewController: UICollectionViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+
+}
+
